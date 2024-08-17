bede9a606bd9729cc390a29ae9cc896c0cfeced8
sheheryartd <sheheryar.bhatti@teradata.com>
2024-08-17 17:27:41 +0500

    Update tests

--- src/index.js
+++ src/index.js
@@ -20,6 +20,16 @@ app.use('/products', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+let server;
+
+sequelize.sync()
+    .then(() => {
+        server = app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.export = server

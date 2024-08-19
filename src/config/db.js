b20330b937272befbdc36281401a30024f79faf2
sheheryartd <sheheryar.bhatti@teradata.com>
2024-08-19 12:41:26 +0500

    Fixed test CI variables

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
 });
 
